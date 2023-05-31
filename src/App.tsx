a80aa8078f9cdda1127d7334ee41ff44118b0fb9
jasonhume <jasonhume@churchofjesuschrist.org>
2023-05-31 07:17:12 -0600

    feat: add mui

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import "./App.css"
 import BookingCalendar from "./BookingCalendar"
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import { Paper } from "@mui/material";
 
 function App() {
   return (
-    <div style={{width: 500}}>
+    <Paper style={{width: 500}}>
       <BookingCalendar />
-    </div>
+    </Paper>
   )
 }
 
